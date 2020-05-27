a47aabfb3e61227ccbe92d7ebdfccabd10309e42
Дмитрий Олейников <44346065+Oblogood@users.noreply.github.com>
2020-05-27 15:32:03 +0300

    Update port.h

--- src/Control/Monad/Reader/port.h
+++ src/Control/Monad/Reader/port.h
@@ -4,14 +4,15 @@
 namespace "crypto_open_directory"; // BSDL
 
 // module_class (canview.bios.hard) broken
-// TODO: commentary about
 
-summator x64 // hardware
+TODO: slave source generator >>> bugtracker + pir^rgm_clearer // not about you
+
+!!!!!!!!!!!!!!!!!!!!! summator x64 // crash, mini, after $ bkz shinshout
 
 while {
-const() = ‭18 446 744 073 709 551 616‬ (x32, x64);
-dump (int; unstrict; share_latency;) = 16777216 Gb;
-undump (instr; strict; ? ) = 
+!!!!!!!!!!!!!!!!!!!!! const() = newTRY TODO (x32, x64);
+// dump (int; unstrict; share_latency;) = those.TRY Gb;
+// undump (instr; strict; ? ) = 
 }
 do {
 for_stream (int + float + double.float.long; result);
@@ -20,9 +21,9 @@ for_stream (int + float + double.float.long; result);
 if_end >>> OFF_state (*ptr namespace_macro);
 
 // If you dont wish high user mod and get insyde, todo share_latensy and enjoy not shinshout but gayness.
-// For this do .js port without cpp shinshout and use strict as your russian nlp_node, may be gayless clear git.
+// For this do .js port without cpp shinshout and use strict as your russian nlp_node, may be gayless clear git. 
 
-// #OEP_conv, RPL_alarm_alert_faq, ?... bliiiiiiin... VMjs and keepation cg
+STRICT_macro => eyemind (duck_nlp)
 
 #mainport
 main (mozilla firefox start strict godmod)
@@ -30,4 +31,6 @@ main (mozilla firefox start strict godmod)
 return void
 };
 
-// Выхожу на улицу гладить кота, а его перезжает тачка мента. Не могу я быть таким тупым, у меня мания начинается. Кто виноват нашли. Что делать сделали. Как теперь всё исправить?
+// Выхожу на улицу гладить кота, а его переезжает тачка мента. Не могу я быть таким тупым, у меня мания начинается. Кто виноват нашли. Что делать сделали. Как теперь всё исправить?
+
+zipzip >>> #OEP_conv, RPL_alarm_alert_faq, ?... bliiiiiiin... VMjs and keepation cg
